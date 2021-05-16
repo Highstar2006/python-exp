009046af755e177750078e30966f09d1c1e0d087
Highstar2006 <62802306+Highstar2006@users.noreply.github.com>
2021-05-16 16:11:09 -0400

    added function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 ##
+
+#1 function with 1 parameter to tell me time of city
+#1 function with 1 parameter to tell me day in city
+import datetime
+
+
+def timeofCity ():
+    return datetime.datetime.now()
+print(timeofCity())
