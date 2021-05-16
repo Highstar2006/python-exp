7b8514ffd514533578e90aff99f1f5b889776455
Highstar2006 <62802306+Highstar2006@users.noreply.github.com>
2021-05-16 16:52:59 -0400

    added 4 functions

--- main.py
+++ main.py
@@ -8,3 +8,33 @@ import datetime
 def timeofCity ():
     return datetime.datetime.now()
 print(timeofCity())
+
+#4functions with 2 parameters,add,subtract,mutipity,divde.
+
+def addnumber (a,b):
+    sum=a+b
+    return sum
+num1 = 12
+num2 = 8
+print(addnumber(num1,num2))
+
+def subract (c,d):
+    sub=c-d
+    return sub
+num3= 12
+num4= 8
+print(subract(num3,num4))
+
+def multiply (e,f):
+    times=e*f
+    return times
+num5= 12
+num6= 8
+print(multiply(num5,num6))
+
+def divde (g,h):
+    times=g/h
+    return times
+num7= 12
+num8= 8
+print(divde(num7,num8))
